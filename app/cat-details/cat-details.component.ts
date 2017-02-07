0b5f2694c84da14c95593367e1f17808c4539421
Sebastian Witalec <sebawita@gmail.com>
2017-02-07 11:40:34 +0000

    Displayed passed params

--- app/cat-details/cat-details.component.ts
+++ app/cat-details/cat-details.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'my-cat-details',
     templateUrl: './cat-details/cat-details.component.html'
 })
 export class CatDetailsComponent implements OnInit{
+  id: number;
 
-  constructor() {
-
-  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-
+    this.id = +this.route.snapshot.params["id"];
   }
 }
--- app/dog-details/dog-details.component.ts
+++ app/dog-details/dog-details.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'my-dog-details',
     templateUrl: './dog-details/dog-details.component.html'
 })
 export class DogDetailsComponent implements OnInit{
+  id: number;
 
-  constructor() {
-
-  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-
+    this.id = +this.route.snapshot.params["id"];
   }
 }
